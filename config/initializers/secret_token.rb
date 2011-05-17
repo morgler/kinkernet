fcc6bc5025c59a3724c8819b5abd7c1ada390591
Matthias Orgler <matthias@dreimannzelt.de>
2011-05-17 18:44:35 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kinkernet::Application.config.secret_token = '6a7a0191570011f07675d5d1e17fcfafa79f40333a50a3cfbe6f9d66e8f9795533934b0445f5633e21859fe9ce8c7f55963c4e9092db05d02eb54ac5171fc637'
